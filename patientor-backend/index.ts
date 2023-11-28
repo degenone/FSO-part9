77cfd964a7bdad1a0a222446e12fed1cc28d80c8
Tero Kilpeläinen <degenone1@gmail.com>
2023-11-28 18:46:04 +0200

    exercise 9.8 done

---
+++ patientor-backend/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+app.get('/api/ping', (_req, res) => res.send('pong'));
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
