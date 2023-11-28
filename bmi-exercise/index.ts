e0797a27dbaeace5b0ec95699b64381897ec515d
Tero Kilpeläinen <degenone1@gmail.com>
2023-11-28 13:28:11 +0200

    exercise 9.4 done

---
+++ bmi-exercise/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+const app = express();
+const PORT = 3003;
+
+app.get('/hello', (_req, res) => res.send('Hello, Full Stack!'));
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
